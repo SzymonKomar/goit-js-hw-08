83c28788e61ec922739db987a9110ed9a95927ce
SzymonKomar <114801537+SzymonKomar@users.noreply.github.com>
2023-03-15 00:43:22 +0100

    update hw 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,35 @@
+const { throttle } = require('lodash');
 let inputInfo = {
   email: '',
   message: '',
 };
-
-const emailInputGet = document.getElementById('emailBox');
-const messageInputGet = document.getElementById('textBox');
-
-emailInputGet.addEventListener('onkeydown', () => {
-  console.log(emailInputGet.innerText);
+let summary = {};
+const emailInputGet = document.querySelector('input');
+const messageInputGet = document.querySelector('textarea');
+const formGet = document.querySelector('form');
+emailInputGet.addEventListener('input', () => {
+  inputInfo = { ...inputInfo, email: emailInputGet.value };
 });
+messageInputGet.addEventListener('input', () => {
+  inputInfo = { ...inputInfo, message: messageInputGet.value };
+});
+formGet.addEventListener(
+  'input',
+  throttle(() => {
+    localStorage.setItem('input_info', JSON.stringify(inputInfo));
+  }, 500)
+);
+const inputInfoGet = JSON.parse(localStorage.getItem('input_info'));
+
+function checkInputStatus() {
+  if (emailInputGet.value == '') {
+    emailInputGet.value = inputInfoGet.email;
+  }
+  if (messageInputGet.value == '') {
+    messageInputGet.value = inputInfoGet.message;
+  }
+}
+emailInputGet.addEventListener('blur', checkInputStatus);
+//Code for auto-callback
+
+const buttonGet = document.querySelector('submit');
