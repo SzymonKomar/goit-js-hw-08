eb82cd59655f49bfe3647adb1e5091dd13455896
SzymonKomar <114801537+SzymonKomar@users.noreply.github.com>
2023-03-14 23:43:48 +0000

    Deploying to gh-pages from @ SzymonKomar/goit-js-hw-08@83c28788e61ec922739db987a9110ed9a95927ce 🚀

--- 03-feedback.69ec0b1a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.getElementById("emailBox");document.getElementById("textBox");e.addEventListener("onkeydown",(function(){console.log(e.innerText)}))}();
-//# sourceMappingURL=03-feedback.69ec0b1a.js.map
--- 03-feedback.b481663b.js
+++
@@ -1,2 +0,0 @@
-const e=document.getElementById("emailBox");document.getElementById("textBox");e.addEventListener("onkeydown",(()=>{console.log(e.innerText)}));
-//# sourceMappingURL=03-feedback.b481663b.js.map
